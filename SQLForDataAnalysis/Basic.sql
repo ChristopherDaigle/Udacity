a7a6fd4257add656826b794f6af734d7001d02d5
Chris Daigle <chris.daigle@uconn.edu>
2019-05-24 17:07:19 -0400

    Push begining of lectures

--- SQLForDataAnalysis/Basic.sql
+++ SQLForDataAnalysis/Basic.sql
@@ -4,3 +4,85 @@ SELECT
 FROM
 	web_events
 LIMIT 15;
+
+/* Including Order By clause */
+SELECT
+	occurred_at, account_id, channel
+FROM
+	web_events
+ORDER BY
+  occurred_at
+LIMIT 15;
+
+SELECT
+  id, occurred_at, total_amt_usd
+FROM
+  orders
+ORDER BY
+  occurred_at
+LIMIT 10;
+
+SELECT
+	id, account_id, total_amt_usd
+FROM
+	orders
+ORDER BY
+	total_amt_usd DESC
+LIMIT 5;
+
+SELECT
+	id, account_id, total_amt_usd
+FROM
+	orders
+ORDER BY
+	total_amt_usd
+LIMIT 20;
+
+/* Including multiple Order By clause */
+SELECT
+	id, account_id, total_amt_usd
+FROM
+	orders
+ORDER BY
+	account_id, total_amt_usd DESC;
+
+SELECT
+	id, account_id, total_amt_usd
+FROM
+	orders
+ORDER BY
+	 total_amt_usd DESC, account_id;
+
+-- The secondary sorting by account ID is difficult
+-- to see here, since only if there were two orders
+-- with equal total dollar amounts would there need
+-- to be any sorting by account ID.
+
+/* Using the Where clause */
+SELECT
+	*
+FROM
+	orders
+WHERE
+	gloss_amt_usd >= 1000
+LIMIT 5;
+
+SELECT
+	*
+FROM
+	orders
+WHERE
+	total_amt_usd < 500
+ORDER BY
+	total_amt_usd DESC
+LIMIT 10;
+
+/* Using WHERE with Non-Numeric Data */
+SELECT
+	name,
+    website,
+    primary_poc
+FROM
+	accounts
+WHERE
+	name = 'Exxon Mobil';
